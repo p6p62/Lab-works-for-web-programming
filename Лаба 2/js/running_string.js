f89065ecd63b2cf05990e0e591b30b1154838d00
Boris <boralgladyshev@gmail.com>
2022-04-20 00:30:20 +0300

    Удалил старые папки с 3 лабами. На общем сайте всё работает

--- Лаба 2/js/running_string.js
+++
@@ -1,7 +0,0 @@
-function startTimer() {
-    var timerBlock = document.getElementById("time_counter");
-    setInterval(() => {
-        var counterValue = parseFloat(timerBlock.textContent);
-        timerBlock.textContent = (counterValue + 0.1).toFixed(1);
-    }, 100);
-}
