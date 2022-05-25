1044ebfec18c885c3eb73a26320b726fd31a0fe8
Boris <boralgladyshev@gmail.com>
2022-05-26 00:53:31 +0300

    Сделал основу под 7 работу

--- js/on_page_loaded.js
+++ js/on_page_loaded.js
@@ -4,8 +4,8 @@ var mainElem = document.getElementsByTagName("main")[0];
 var newMargin = document.getElementById("lab-work-menu").clientWidth + INTERVAL_PX;
 mainElem.style.marginLeft = newMargin + "px";
 
-// активация отображения шестой лабораторки
-changeLabWorkOnPage("content_lab_6");
+// активация отображения седьмой лабораторки
+changeLabWorkOnPage("content_lab_7");
 
 // установка одинаковой ширины кнопкам меню
 let allButtons = document.getElementById('lab-work-menu').getElementsByTagName('button');
