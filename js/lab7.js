5165a9f3bfa0d3dd083bf2b40d4b83e7cad2d350
Boris <boralgladyshev@gmail.com>
2022-05-27 16:19:49 +0300

    Сделал 3 математических функции

---
+++ js/lab7.js
@@ -0,0 +1,11 @@
+function f1(x) {
+    return (Math.pow(x, 3) + 2 * x) / (4 * Math.sin(4 * x)) + Math.pow(Math.tan(4 * x), 3);
+}
+
+function f2(x) {
+    return Math.pow(Math.sin(x), 2) - Math.abs(Math.log(x) + Math.exp(x)) / 4;
+}
+
+function f3(x) {
+    return Math.sqrt(Math.pow(x, 3) + 6 * x) / (Math.pow(x, 5) - 4 * x);
+}
